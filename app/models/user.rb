bab7be9e447839b7b3615fd61f7743ab7a53ea12
Kylan Wong <mynameiskylan@gmail.com>
2021-02-08 12:39:16 -0800

    add friendship/friends associations and method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :validatable
+
+  has_many :forward_friendships, class_name: 'Friendship', foreign_key: :user_id
+  has_many :forward_friends, through: :forward_friendships, source: :friend
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  def friends
+    forward_friends + inverse_friends
+  end
 end
