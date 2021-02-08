e5630ec318acc568b9dc8c2f7d450ffe5d40ac6a
Kylan Wong <mynameiskylan@gmail.com>
2021-02-08 13:01:56 -0800

    add auth to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,38 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user_from_token!
+
+  def authenticate_user_from_token!
+    auth_token = request.header['Authorization']
+
+    if auth_token
+      authenticate_with_auth_token(auth_token)
+    else
+      authentication_error
+    end
+  end
+
+  private
+
+  def authenticate_with_auth_token(auth_token)
+    unless auth_token.include?(':')
+      authentication_error
+      return
+    end
+
+    user_id = auth_token.split(':').first
+    user = User.where(id: user_id).first
+
+    if user && Devise.secure_compare(user.access_token, auth_token)
+      # user can access
+      sign_in(user, store: false) # don't know what store: false does yet
+    else
+      authentication_error
+    end
+  end
+
+  # Authentication Failure
+  # Renders a 401 error
+  def authentication_error
+    # User's stoken is either invalid or not the right format
+    render json: {error: t('unauthorized')}, status: 401 # Authentication timeout
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :validatable
 
+  after_create :update_access_token!
+
   has_many :forward_friendships, class_name: 'Friendship', foreign_key: :user_id
   has_many :forward_friends, through: :forward_friendships, source: :friend
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
@@ -10,5 +12,11 @@ class User < ApplicationRecord
 
   def friends
     forward_friends + inverse_friends
+  end
+
+  private
+
+  def update_access_token!
+    self.access_token = "#{self.id}:#{Devise.friendly_token}"
   end
 end
