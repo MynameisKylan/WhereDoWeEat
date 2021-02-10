fa92958a2acc1934ec49983ae11e1adb052539f0
Kylan Wong <mynameiskylan@gmail.com>
2021-02-10 11:30:16 -0800

    react setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={} />
+      <Route exact path="/" component={} />
+    </Switch>
+  );
+};
+
+export default App;
