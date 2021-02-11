64b3052ef0e18e7ca3dba44bdbc528cec050d8e4
Kylan Wong <mynameiskylan@gmail.com>
2021-02-11 13:19:09 -0800

    overwrite devise sessions methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < Devise::SessionsController
   skip_before_action :authorized
+  respond_to :json
 
   # Rails looks in same namespace for serializers (ex: serializers/session_serializer.rb)
   def respond_with(resource, opts = {})
     token = encode_token({ user_id: resource.id })
     render json: { user: resource, token: token }
+  end
+
+  # # Overwrite create to stop redirect
+  def create
+    self.resource = warden.authenticate(auth_options)
+    if self.resource
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource
+    else
+      render json: { error: "Incorrect Email or Password" }
+    end
   end
 end
